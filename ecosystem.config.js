34c8d617e3f447dfdb12cbf91b83d44b033b9f88
Mirko965 <mirkojelic.jelic@gmail.com>
2019-04-25 01:26:33 +0200

    deploy on aws centos7

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       host: 'ec2-34-195-199-8.compute-1.amazonaws.com',
       key: '~/.ssh/centos7.pem',
       ref: 'origin/aws',
-      repo: 'git@github.com:Mirko965/my-props.git',
+      repo: 'https://github.com/Mirko965/my-props.git',
       path: '/home/centos/my-props',
       'post-deploy': 'npm install && cd ./client && npm install && npm run build && cd ../ && pm2 startOrRestart ecosystem.config.js'
     }
